524fca49368b459a382a9fbf92874b5e9ae24aab
dam <damien.bais@gmail.com>
2017-04-26 15:15:03 +0200

    construction interface

--- client/main.js
+++ client/main.js
@@ -6,6 +6,7 @@ import './main.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  Session.set("MODE","none");
 });
 
 Template.hello.helpers({
@@ -14,9 +15,35 @@ Template.hello.helpers({
   },
 });
 
-Template.hello.events({
+
+
+
+Template.body.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+  }
+  ,
+  'click #source'(event, instance) {
+    // increment the counter when button is clicked
+    console.log("source!");
+    Session.set("MODE","source");
   },
+  'click #generation'(event, instance) {
+    // increment the counter when button is clicked
+    console.log("generation!");
+    Session.set("MODE","generation");
+  }
+});
+
+
+Template.body.helpers({
+    getMode(who){
+        var r = Session.get("MODE");
+        if(r==who){
+            return true;
+        }else{
+            return false;
+        }
+    },
 });
