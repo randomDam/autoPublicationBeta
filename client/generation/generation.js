b5f2ef0db9b06472c58c62b9d73f22ab2ee5bcdc
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2017-06-28 20:35:03 +0200

    PDF aleatoire ok

--- client/generation/generation.js
+++ client/generation/generation.js
@@ -58,21 +58,31 @@ data = new ReactiveVar([])
 
 Template.generation.events({
   'click .myButton': function() {
-
-    var pages = Pages.find().map(function(page){
-      var p = [];
+    var pagesCount = Pages.find().count();
+    var pages = Pages.find().map(function(page,indexPage){
       var nbSources = page.sources.length;
+
+      var orientation = ""
+      var ori = Math.floor(Math.random() * 2)
+      if(ori == 0) orientation = "v";
+      else orientation = "h";
+
+      var gabarit = svgGabarit.get();
+      console.log(orientation, gabarit)
+      var p = [];
+
       if(nbSources == 1){
         // Une seule source sur cette page
-        var r = Math.floor((Math.random() * 3))
-        p.push(svgGabarit.get().h[1][r]);    
+        var r = Math.floor((Math.random() * gabarit[orientation][1].length ))
+        p.push(gabarit[orientation][1][r]);    
       }
       if(nbSources == 2){
         // Deux sources sur cette page
-        var r = Math.floor((Math.random() * 3))
-        p.push(svgGabarit.get().v[2][r][0]);    
-        p.push(svgGabarit.get().v[2][r][1]);    
+        var r = Math.floor((Math.random() * gabarit[orientation][2].length))
+        p.push(gabarit[orientation][2][r][0]);    
+        p.push(gabarit[orientation][2][r][1]);    
       }
+
       sources = Pages.rawData(page._id).map(function(s,i){
         x = parseFloat(p[i].x);
         y = parseFloat(p[i].y-p[i].height);
@@ -80,14 +90,17 @@ Template.generation.events({
         h = parseFloat(p[i].height);
 
         return {
-          image: s.rawData , absolutePosition: {x:x, y: y}, width : w, height : h //, absolutePosition:[200,500] //s.content
+          image: s.rawData , absolutePosition: {x:x, y: y}, fit:[w,h] //width : w, height : h //, absolutePosition:[200,500] //s.content
         }
       })
-      return [
-        {text: "A", absolutePosition : {x:53.14,y:646.67}},
-        sources ,
-        {text: page._id, pageBreak: 'after'}
-      ]
+      if(sources.length == 0){
+        return [ {text: page._id, pageBreak: 'after'} ]
+      
+      } else {
+        if(indexPage < pagesCount) b = "after";
+        else b = "";
+        return [ sources , {text: page._id, pageBreak: b} ]
+      }
     });
     
 
--- client/main.js
+++ client/main.js
@@ -27,7 +27,7 @@ Template.body.onRendered(function(){
     var data = result.svg.g;
     var index = 0;
     _.each(data, function(layer){
-      console.log(index, layer);
+      console.log(index, layer.$["inkscape:label"]);
       index++;
     });
 
@@ -36,35 +36,14 @@ Template.body.onRendered(function(){
     var o = {
       "h" : {
         "1" : [data[1].rect[0].$,data[2].rect[0].$,data[3].rect[0].$,data[4].rect[0].$],
-        "2" : []
+        "2" : [[data[8].rect[0].$,data[8].rect[1].$],[data[9].rect[0].$,data[9].rect[1].$],]
       },
       "v" : {
         "1" : [data[5].rect[0].$],
-        "2" : [[data[6].rect[0].$,data[6].rect[1].$],[data[7].rect[0].$,data[7].rect[1].$],[data[8].rect[0].$,data[8].rect[1].$],[data[9].rect[0].$,data[9].rect[1].$]]
+        "2" : [[data[6].rect[0].$,data[6].rect[1].$],[data[7].rect[0].$,data[7].rect[1].$]]
       }
     }
     console.log(o);
-
-    /*
-    for(i=1 ; i < 5 ; i++){
-      var d = data[i].rect[0].$;
-      d.x = parseFloat(d.x);
-      d.y = parseFloat(d.y);
-      d.width = parseFloat(d.width);
-      d.height = parseFloat(d.height);
-      o.h["1"][i-1] = d
-    }
-
-    for(i=1 ; i < 5 ; i++){
-      var d = data[i].rect[0].$;
-      d.x = parseFloat(d.x);
-      d.y = parseFloat(d.y);
-      d.width = parseFloat(d.width);
-      d.height = parseFloat(d.height);
-      o.v["2"][i-6] = d
-    }
-    */
-
     svgGabarit.set(o);
   });
 
